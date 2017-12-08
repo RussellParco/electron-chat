eaf04f39b188ef4561e551e2b531005a321637a6
JScrivo <jordan.scrivo@gmail.com>
2017-12-07 23:18:29 -0500

    Implimented chat creation and member addition

--- messager.js
+++ messager.js
@@ -7,7 +7,7 @@ function getMessages(ID){
 
 }
 
-function updateSelecton(){
+function updateSelecton(){  //change this to use POST
   var xhttp;
   xhttp = new XMLHttpRequest();
 
@@ -39,11 +39,88 @@ function updateSelecton(){
   xhttp.send();
 }
 
-function changeChats(chatID){
+function changeChats(chatID){ //change or remove this
   if (chatID == "") {
     document.getElementById("test").innerHTML = 'Select a chat to begin';
   }
   else {
     document.getElementById("test").innerHTML = 'input from option: ' + chatID;
+  }
+}
+
+function createChat() {
+  var newChatname = document.getElementById("createchatname").value;
+
+  if (newChatname == ""){
+    alert("Please enter a chat name");
+  }
+  else {
+    var request = new XMLHttpRequest();
+    var url = "http://jordanscrivo.ga/createchat";
+    request.open("POST", url, true);
+    request.setRequestHeader("Content-type", "application/json");
+    request.onreadystatechange = function () {
+      if (request.readyState === 4 && request.status === 200) {
+        json = JSON.parse(request.responseText);
+        //alert(json.Status);
+        var list = document.getElementById("chatselect");
+        var option = document.createElement('option');
+        option.text = newChatname;
+        option.value = json.chatID;
+        list.add(option);
+        //handle responseText
+
+      } else if (request.status === 401 && request.readyState === 4) {
+        alert("Server returned 401");
+      } else if (request.readyState === 4 && request.status === 500) {
+        alert("Server returned 500");
+      } else if (request.status === 400 && request.readyState === 4) {
+        alert("Server returned 400");
+      }
+    }
+    var data = JSON.stringify({"newChatname": newChatname, "Token": Token});
+    request.send(data);
+
+    document.getElementById("createchatname").value = "";
+  }
+}
+
+function addMember() {
+  var memberUsername = document.getElementById("addmemberusername").value;
+  var chatselect = document.getElementById("chatselect");
+  var chatID = chatselect.options[ chatselect.selectedIndex ].value;
+
+  if (memberUsername == ""){
+    alert("Please enter a username");
+  } else if (chatID == ""){
+    alert("Please select a chat before attempting to add a member");
+  }
+  else {
+    var request = new XMLHttpRequest();
+    var url = "http://jordanscrivo.ga/addmember";
+    request.open("POST", url, true);
+    request.setRequestHeader("Content-type", "application/json");
+    request.onreadystatechange = function () {
+      if (request.readyState === 4 && request.status === 200) {
+        json = JSON.parse(request.responseText);
+        //alert(json.Status);
+        var list = document.getElementById("chatselect");
+        var option = document.createElement('option');
+        option.text = newChatname;
+        option.value = json.chatID;
+        list.add(option);
+        //handle responseText
+
+      } else if (request.status === 401 && request.readyState === 4) {
+        alert("Server returned 401");
+      } else if (request.readyState === 4 && request.status === 500) {
+        alert("Server returned 500");
+      } else if (request.status === 400 && request.readyState === 4) {
+        alert("User does not exist or is already a member of this chat");
+      }
+    }
+    var data = JSON.stringify({"chatID": chatID, "Token": Token, "memberUsername":memberUsername});
+    request.send(data);
+    document.getElementById("addmemberusername").value = "";
   }
 }
